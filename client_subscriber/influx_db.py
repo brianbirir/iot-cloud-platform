abd4d9aafa4b2e67b5fab321725fad4c0678ffea
Brian Birir <brianbirir@gmail.com>
2017-05-14 10:33:51 +0300

    Add improvements
    * import son module
    * create functions for checking database existence, posting data to InfluxDB

--- client_subscriber/influx_db.py
+++ client_subscriber/influx_db.py
@@ -1,46 +1,65 @@
 # this module pushes data from the subscriber to Influx DB
 
 from influxdb import InfluxDBClient
+import json
 
-# using Http to connect to InfluxDB database
-db_name = 'ruleblox'
+# using HTTP to connect to InfluxDB database
+db_name = 'ruleblox_production'
 db_host = '138.197.6.61'
 db_port = 8086
 db_user = 'admin'
 db_password = 'ruleblox@2017#!'
 
+# instantiate influx db client
 client_db = InfluxDBClient(db_host, db_port, db_user, db_password)
 
-# get all databases
-all_dbs_list = client_db.get_list_database()
+# check if DB exists, if not create it
+def check_DB():
+    # get all databases
+    all_dbs_list = client_db.get_list_database()
 
-# check if current database exists and if not create it
-if db_name not in [str(x['name']) for x in all_dbs_list]:
-    print "Creating db {0}".format(db_name)
-    client_db.create_database(db_name)
-else:
-    print "Reusing db {0}".format(db_name)
+    # check if current database exists and if not create it
+    if db_name not in [str(x['name']) for x in all_dbs_list]:
+        print "Creating db {0}".format(db_name)
+        client_db.create_database(db_name)
+    else:
+        print "Reusing db {0}".format(db_name)
 
-client_db.switch_database(db_name)
+    client_db.switch_database(db_name)
 
-# data in JSON format
-json_body = [
-    {
-        "measurement": "cpu_load_short",
+# store sensor data
+def sensor_data_handler(sensorData):
+    # parse the json data
+    json_Dict = json.loads(sensorData)
+    SensorID = json_Dict['Sensor_ID']
+    Temperature = json_Dict['Temperature']
+    Humidity = json_Dict['Humidity']
+
+    # json data
+    json_body = [
+        {
+            "measurement": "room",
             "tags": {
-                "host": "server01",
-                "region": "us-west"
+                "device_id": SensorID,
             },
-            "time": "2009-11-10T23:00:00Z",
             "fields": {
-                "value": 0.64
+                "temperture": Temperature,
+                "humidity": Humidity
             }
-    }
-]
+        }
+    ]
 
-# write points
-client_db.write_points(json_body)
+    # check database existence
+    check_DB()
 
-result = client_db.query('select value from cpu_load_short;')
+    # write points
+    client_db.write_points(json_body)
 
-print("Result: {0}".format(result))
+
+# function to collect data from subscriber
+def sensor_handler(topic, jsonSensorData):
+
+    if topic == 'Room/Si7021':
+        sensor_data_handler(jsonSensorData)
+    else:
+        print "No topic to subscribe to!"
