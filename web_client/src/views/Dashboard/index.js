230e3d9f681c97f062fe80fbae66d85fc83137d4
brianbirir <brianbirir@gmail.com>
2019-08-16 12:58:41 +0300

    feat(dashboard): Implement demo dashboard

---
+++ web_client/src/views/Dashboard/index.js
@@ -0,0 +1,39 @@
+import React, { Component, lazy, Suspense } from "react";
+import { Bar, Line } from "react-chartjs-2";
+import {
+  Badge,
+  Button,
+  ButtonDropdown,
+  ButtonGroup,
+  ButtonToolbar,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  Col,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Progress,
+  Row,
+  Table
+} from "reactstrap";
+import { CustomTooltips } from "@coreui/coreui-plugin-chartjs-custom-tooltips";
+import { getStyle, hexToRgba } from "@coreui/coreui/dist/js/coreui-utilities";
+import SensorChart from "./SensorChart";
+
+const Widget03 = lazy(() => import("../Widgets/Widget03"));
+
+const brandPrimary = getStyle("--primary");
+const brandSuccess = getStyle("--success");
+const brandInfo = getStyle("--info");
+const brandWarning = getStyle("--warning");
+const brandDanger = getStyle("--danger");
+
+const Dashboard = () => {
+  return <SensorChart />;
+};
+
+export default Dashboard;
