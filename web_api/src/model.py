6f0406bf04fba8f2ef1cd7e210292ff6f788d596
brianbirir <brianbirir@gmail.com>
2019-08-24 15:09:34 +0300

    fix(model): add correct table name
    - Fixes this error:
    sqlalchemy.exc.NoReferencedTableError: Foreign key associated with column 'Devices.project_id' could not find table 'project' with which to generate a foreign key to target column 'id'

--- web_api/src/model.py
+++ web_api/src/model.py
@@ -63,7 +63,7 @@ class DeviceModel(BaseModel):
     __tablename__ = 'Devices'
     name = db.Column(db.String(128), nullable=False)
     device_uuid = db.Column(db.String(128), nullable=False)
-    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('Project.id'), nullable=False)
 
     def __repr__(self):
         return '<name: {} >'.format(self.name)
