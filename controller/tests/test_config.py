d4f70c19517055bb49ed0e69e7f55691047fa027
brianbirir <brianbirir@gmail.com>
2019-10-15 09:43:32 +0300

    chore(test): remove unused unit test
    - we are now obtaining env values from within Docker container

--- controller/tests/test_config.py
+++ controller/tests/test_config.py
@@ -8,9 +8,6 @@ class ConfigTest(unittest.TestCase):
 
     c = Config()
 
-    def test_env_path(self):
-        self.assertTrue(os.path.exists(self.c.env_file))
-
     def test_env_variables(self):
         self.assertEquals(self.c.BROKER_PORT, "1883")
 
