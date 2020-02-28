abed98ad438cdd54925a090fa1e11d3619e7763b
brianbirir <brianbirir@gmail.com>
2020-02-28 23:07:50 +0300

    Random changes

--- web_api/config.py
+++ web_api/config.py
@@ -4,18 +4,15 @@ from dotenv import load_dotenv
 
 class Config:
     """Aggregates configuration variables for the application"""
-    env_file_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    env_file = env_file_path + '/' + '.env'
-    load_dotenv(dotenv_path=env_file)
 
     DEBUG = False
     TESTING = False
     SECRET_KEY = str(os.getenv('SECRET_KEY'))
     SQLALCHEMY_DATABASE_URI = str(os.getenv('DATABASE_URI'))
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-    INFLUX_DB = os.getenv('INFLUX_DB')
-    INFLUX_USER = os.getenv('INFLUX_USER')
-    INFLUX_PASSWORD = os.getenv('INFLUX_PASSWORD')
+    INFLUX_DB = os.getenv('INFLUXDB_DB')
+    INFLUX_USER = os.getenv('INFLUXDB_ADMIN_USER')
+    INFLUX_PASSWORD = os.getenv('INFLUXDB_ADMIN_PASSWORD')
     INFLUX_PORT = os.getenv('INFLUX_PORT')
     INFLUX_HOST = os.getenv('INFLUX_HOST')
     INFLUX_DB_MEASUREMENT = os.getenv('INFLUX_DB_MEASUREMENT')
@@ -36,8 +33,8 @@ class TestingConfig(Config):
 
 
 class DatabaseInitializationConfig(Config):
-    POSTGRES_DATABASE_NAME = os.getenv('POSTGRES_DATABASE_NAME')
-    POSTGRES_DATABASE_USER = os.getenv('POSTGRES_DATABASE_USER')
+    POSTGRES_DATABASE_NAME = os.getenv('POSTGRES_DB')
+    POSTGRES_DATABASE_USER = os.getenv('POSTGRES_USER')
     POSTGRES_DATABASE_HOST = os.getenv('POSTGRES_DATABASE_HOST')
     POSTGRES_DATABASE_PORT = os.getenv('POSTGRES_DATABASE_PORT')
-    POSTGRES_DATABASE_PASSWORD = os.getenv('POSTGRES_DATABASE_PASSWORD')
+    POSTGRES_DATABASE_PASSWORD = os.getenv('POSTGRES_PASSWORD')
